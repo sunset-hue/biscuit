ee793931d7c8766d6e1c3d9ae4e2497706dc26e1
sunset-hue <sunsetree@proton.me>
2025-07-16 17:11:44 -0700

    fixed up values for each segment

--- biscuit/src/gdt.rs
+++ biscuit/src/gdt.rs
@@ -8,10 +8,10 @@ fn write_all_non_tss_segments(gdtr: TableDescriptor,tss_location: u64) {
     let mut address = gdtr.location as *mut u64;
     unsafe {    
         address.offset(0 as isize) = 0;
-        address.offset(8 as isize) = 0xfffff + 0x9A + 0xA; //this is done to simplify the process of making segments
-        address.offset(16 as isize) = 0xfffff + 0x92 + 0xC;
-        address.offset(24 as isize) = 0xfffff + 0xFA + 0xA;
-        address.offset(32 as isize) = 0xfffff + 0xF2 + 0xC;
+        address.offset(8 as isize) = 0x1000A3; //this is done to simplify the process of making segments
+        address.offset(16 as isize) = 0x10009D;
+        address.offset(24 as isize) = 0x100103;
+        address.offset(32 as isize) = 0x1000FD;
     }
 }
 
