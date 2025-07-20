80b3420b5135677fc103fed68a70b72007655ad3
sunset-hue <sunsetree@proton.me>
2025-07-20 11:49:38 -0700

    actually starting on putting characters on screen rn

--- biscuit/src/framebuffer.rs
+++ biscuit/src/framebuffer.rs
@@ -1,14 +1,27 @@
-use limine::{framebuffer, BaseRevision};
+use limine::{file::File, framebuffer, BaseRevision};
 
 
 
-fn print() {
+
+
+
+
+
+
+fn print(text: str) {
     let revision = BaseRevision::new();
     let framebuffer = limine::request::FramebufferRequest::new();
-    let mut response = framebuffer.get_response().unwrap().framebuffers()
-                                        .last()
-                                        .unwrap()
-                                        .addr();
+    let mut response = framebuffer.get_response().unwrap().framebuffers().last().unwrap().addr();
+    let bytes = include_bytes!("font.bin");
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    for char in text.chars() {
+        let index = alphabet.iter().position(|x| x==char).unwrap();
+
+        
+
+    }
+
+    
     
     // selecting last since I'm only gonna support one display rn
 }
